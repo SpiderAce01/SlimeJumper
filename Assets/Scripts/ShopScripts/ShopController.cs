3611ca0345db05ec47a23e5d70a8646520e5a84a
wrobe71279 <85209846+wrobe71279@users.noreply.github.com>
2022-04-23 16:36:44 +1000

    Alec's Update
    - Implemented Shop with Temporary Skins
    - Implemented Currency system
    - Populated L1 and L2 with Coins to collect

--- Assets/Scripts/ShopScripts/ShopController.cs
+++ Assets/Scripts/ShopScripts/ShopController.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ShopController : MonoBehaviour
 {
-    [SerializeField] private Image selectedSkin;
-    [SerializeField] private Text coinsText;
+    //[SerializeField] private Image selectedSkin;
+    [SerializeField] private TMP_Text coinsText;
     [SerializeField] private SkinManager skinManager;
 
     // Update is called once per frame
     void Update()
     {
         coinsText.text = "Coins: " + PlayerPrefs.GetInt("Coins");
-        selectedSkin.sprite = skinManager.GetSelectedSkin().sprite;
+        //selectedSkin.sprite = skinManager.GetSelectedSkin().sprite;
     }
 
     public void LoadGame() => SceneManager.LoadScene("GameScene");
